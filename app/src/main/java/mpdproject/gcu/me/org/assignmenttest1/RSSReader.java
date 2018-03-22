a951aa305ff9a79411a7dcd7af23203a77b73b7d
fmcfar200 <fmcfar200@caledonian.ac.uk>
2018-03-22 23:14:03 +0000

    New RSS class

--- app/src/main/java/mpdproject/gcu/me/org/assignmenttest1/RSSReader.java
+++ app/src/main/java/mpdproject/gcu/me/org/assignmenttest1/RSSReader.java
@@ -1,30 +1,7 @@
 package mpdproject.gcu.me.org.assignmenttest1;
 
-import android.app.DatePickerDialog;
-import android.app.ProgressDialog;
-import android.app.SearchManager;
-import android.content.ClipData;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -36,222 +13,41 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.sql.Time;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 
-public class ItemViewer extends AppCompatActivity{
+/**
+ * Created by Fraser on 22/03/2018.
+ */
 
-
-    ///private String url1 = "https://www.nasa.gov/rss/dyn/breaking_news.rss";
-    private String url1 = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
-    private String url3 = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
+public class RSSReader {
 
     private String theURL;
+    private int fetchType;
 
-    private String result = "";
-
-    Toolbar toolbar;
-
-    List<RoadWorksItem> rwList = new ArrayList<>();
-    TrafficAdapter adapter;
-
-
-    Button dateButton;
-    ListView listView;
-
-    ProgressDialog theDialog;
-    DatePickerDialog.OnDateSetListener dateSetListener;
-
-
-    protected void onCreate(Bundle savedInstanceState)
+    public RSSReader(String url, int fetchType)
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_viewer);
-
-        //dateButton = (Button) findViewById(R.id.datebutton);
-        //dateButton.setOnClickListener(this);
-
-        listView = (ListView)findViewById(R.id.listView);
-
-        toolbar = (Toolbar)findViewById(R.id.toolbarID);
-        setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle(null);
-
-        Bundle extras = getIntent().getExtras();
-        int fetchType = extras.getInt("FetchType");;
-        if (fetchType == 1)
-        {
-            startProgress(url1, fetchType);
-            getSupportActionBar().setTitle("Current Incidents");
-        }
-        else
-        {
-            startProgress(url3, fetchType);
-            getSupportActionBar().setTitle("Planned Roadworks");
-
-        }
-
-
-        theDialog = new ProgressDialog(ItemViewer.this);
-        theDialog.setMessage("Getting Traffic Info...");
-        theDialog.show();
-
-        dateSetListener = new DatePickerDialog.OnDateSetListener()
-        {
-            @Override
-            public void  onDateSet(DatePicker picker, int year, int month, int day)
-            {
-                month = month + 1;
-
-                String dateString = Integer.toString(day)
-                        + Integer.toString(month) + Integer.toString(year);
-
-                SimpleDateFormat df = new SimpleDateFormat("ddMyyyy");
-                SimpleDateFormat df2 = new SimpleDateFormat("dd MMMM yyyy");
-
-
-
-                try {
-                    Date mySearchDate = df.parse(dateString);
-                    String theSearchDate = df2.format(mySearchDate);
-
-                    List<RoadWorksItem> newList = new ArrayList<RoadWorksItem>();
-                    for (RoadWorksItem item: rwList)
-                    {
-                        if (item.startDate.equals(theSearchDate))
-                        {
-                            newList.add(item);
-                        }
-                    }
-
-                    adapter = new TrafficAdapter(getApplicationContext(),R.layout.listview_item_layout,newList);
-                    listView.setAdapter(adapter);
-
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-
-
-
-            }
-        };
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-        {
-            @Override
-            public void onItemClick(AdapterView<?> adapter, View view, int position, long id)
-            {
-                RoadWorksItem item = (RoadWorksItem)adapter.getItemAtPosition(position);
-                StartFullDetail(item);
-
-            }
-        });
-
-
-
+        theURL = url;
+        fetchType = fetchType;
     }
 
-    private void StartFullDetail(RoadWorksItem item)
-    {
-        Intent i = new Intent(getApplicationContext(), MapsActivity.class);
-        i.putExtra("RoadWorksItem",item);
-        startActivity(i);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.viewer_menu,menu);
-
-        final SearchView searchView = (SearchView)MenuItemCompat.getActionView(menu.findItem(R.id.searchIcon));
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                adapter.getFilter().filter(newText);
-
-                return false;
-            }
-        });
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        int id = item.getItemId();
-
-        if (id == R.id.calendarIcon)
+    public List<RoadWorksItem> runReader()
         {
-            Calendar calendar = Calendar.getInstance();
-            int year = calendar.get(Calendar.YEAR);
-            int month = calendar.get(Calendar.MONTH);
-            int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-            DatePickerDialog dDialog = new DatePickerDialog(
-                    this,
-                    R.style.Theme_AppCompat_Light_Dialog_MinWidth,
-                    dateSetListener,
-                    year,month,day);
-
-            dDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-
-            dDialog.show();
-        }
-        else if (id == R.id.searchIcon)
-        {
-
-        }
-        return true;
-    }
-
-
-
-    public void startProgress(String url, int fetchType) {
-
-        new Thread(new Task(url, fetchType)).start();
-
-
-    }
-
-    class Task extends AsyncTask implements Runnable {
-        private String url;
-        private int fetchType;
-
-
-        public Task(String aurl, int afetchType) {
-            url = aurl;
-            fetchType = afetchType;
-        }
-
-
-        @Override
-        public void run() {
             RoadWorksItem rwItem = null;
-            rwList = new ArrayList<>();
+             List<RoadWorksItem> rwList = new ArrayList<>();
 
             URL aurl;
             URLConnection yc;
             BufferedReader in = null;
             String inputLine = "";
+            String result = "";
 
             try {
-                aurl = new URL(url);
+                aurl = new URL(this.theURL);
                 yc = aurl.openConnection();
                 in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
 
@@ -397,32 +193,6 @@ public class ItemViewer extends AppCompatActivity{
                 }
             }
 
-
-            ItemViewer.this.runOnUiThread(new Runnable() {
-                public void run() {
-                    adapter = new TrafficAdapter(getApplicationContext(),R.layout.listview_item_layout,rwList);
-                    listView.setAdapter(adapter);
-
-                    theDialog.dismiss();
-
-                }
-
-            });
-
-
+            return rwList;
         }
-        @Override
-        protected void onPreExecute()
-        {
-
-        }
-
-        @Override
-        protected Object doInBackground(Object[] params) {
-            return null;
-        }
-
-
-    }
-
 }
